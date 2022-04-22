76046af67dc3418300852d1255424b11ef554249
mjs331 <mjthewiz@gmail.com>
2022-04-21 23:06:23 -0700

    finshed

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Systems-Final
 
-### The purpose of this is to have a site where members of the 321st Imperial to mark what shirts they want from the merch. They will mark their size (S/M/L/XL/2XL) and then see the summary of what shirts are left. 
+### The purpose of this is to have a site where members of the 321st Imperial to mark what shirts they want from the merch. They will mark their size (S/M/L/XL/2XL) and then see the summary of what shirts are left. The Service can be accessedby going to 54.202.242.76 port 5000.
--- routing.py
+++ routing.py
@@ -1,9 +1,56 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
+import sqlite3 as sql
+
 app = Flask(__name__)
 
 @app.route ("/")
 def error():
     return "If you are seeing this than something is wrong"
+
+@app.route ("/welcome")
+def welcome():
+    return "If you are seting this text than the page failed to load. Pleaste refeash. If issue persists constact the page owner at mjthewiz@gmail.com"
+
+@app.route ("/Shirts")
+def shirts():
+        return render_template ("Shirts.html")
+
+@app.route ("/bdfields", methods = ["POST", "Get"])
+def dbfields:
+        if request.method == "POST":
+            name = request.form ["name"]
+            size = request.form ["size"]
+            color = request.form ["color"]
+
+        with sql.connect("database.db") as con:
+            cur = con.cursor():
+            cur.execute ("INSERT INTO shirts (name, size, color) VALUES ('{0}','{1}', '{2}', '{3}'".format(shirt,size,color))
+            con.commit ()
+                message = "Your choice has been recorded! Thank you!"
+    
+        return render_template ("result.htm", msg = message):
+
+@app.route ("/shirts", methods = ["POST", "GET"])
+def shirts():
+    if request.method == "Post":
+        shirt = request.form["shirts"]
+        color = request.form ["color"]
+        size = request.form ["color"]
+    with sql.connect ("databases.db") as con:
+        con.commit ()
+                message = "Your choice has been recorded! Thank you!"
+    return render_template ("result.htm", msg = message):
+
+@app.route ("/inventory")
+def inventory():
+    con = sql connect("database.db")
+    con.row_factory = sql.Row
+
+    cur = con.cursor()
+    cur.execure ("SELECT * from shirts")
+
+    rows = cur.fetchall()
+    return render_template ("inventory.htm", rows = rows)
 
 if __name__=="__main__":
     app.run(debug=True)
